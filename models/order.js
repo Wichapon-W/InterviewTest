ca5b08ddb7aa119df405cfae7f2f8dc0e6c785c5
Wichapon-W <wichapon.wt@gmail.com>
2025-04-24 04:31:44 +0000

    mongoose

--- models/order.js
+++ models/order.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 const orderSchema = new mongoose.Schema({
     customer_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Customer' },
     product_name: String,
--- models/transaction.js
+++ models/transaction.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 const transactionSchema = new mongoose.Schema({
     title: String,
     type: { type: String, enum: ['income', 'expense'] },
