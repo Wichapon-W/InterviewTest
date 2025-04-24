b161d416b483bf145972a929f717e09234326a81
Wichapon-W <wichapon.wt@gmail.com>
2025-04-24 04:40:30 +0000

    mongose default

--- server.js
+++ server.js
@@ -25,12 +25,11 @@ app.use('/transactions', transactionRoutes);
 app.use('/dashboard', dashboardRoutes);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/customer_wallet', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log('Connected to MongoDB');
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}).catch(err => {
-  console.error('MongoDB connection error:', err);
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/customer_wallet')
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+  });
